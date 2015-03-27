c3ea343678f8b75e9cd7618b159449c85dfa49b2
PawelSz <peal1982@gmail.com>
2015-03-27 18:17:48 +0100

    Dodaj HMTL required max w walidation

--- application/views/nowa_strona_form.php
+++ application/views/nowa_strona_form.php
@@ -8,7 +8,7 @@
 	<form class="form_vraper" action='{przycisk_zapisz_akcja_do_wykonania}' method='post' accept-charset="utf-8">
 		<?php echo validation_errors();?>
 		Tytul artykulu:<br>	
-		<input type="text" name="tytul" class="small_form" value='{tytul_artykulu}' ><br><br>
+		<input type="text" name="tytul" class="small_form" value='{tytul_artykulu}' max="255" required><br><br>
 		Wpisz nowy artykul:<br>
 		<textarea name="tresc"  class="big_form" >{tekst}</textarea>
 		<br>
--- application/views/zaloguj.php
+++ application/views/zaloguj.php
@@ -5,9 +5,9 @@
 	<?php echo validation_errors();?>
 	{form_start}
 	Login:<br>	
-	<input type="text" name="login" class="small_form"><br><br>
+	<input type="text" name="login" class="small_form"  max="64" required><br><br>
 	Wpisz haslo:<br>
-	<input type="password" name="haslo" class="small_form"><br><br>
+	<input type="password" name="haslo" class="small_form" max="128" required><br><br>
 	<br>
 	<input type="submit" value="Zaloguj">
 	<br>
